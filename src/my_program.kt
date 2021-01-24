15938424aa39db2bc5effa3577df1b0b5a7b98f8
Nikolay Lenovo Home <gemansipator@gmail.com>
2021-01-24 16:30:42 +0400

    fun calcTax(sum:Int = 1000): Double{     //число 1000 от балды     val tax_ratio = 0.13
        return tax_ratio*sum
    }
    
    fun main() {
        val sum_user: String? = readLine()
    
        if (sum_user == null) return   //Если значение пустое, то возвратит return и покинет функцию ТОЧКА ВХОДА
        // и приложение завершится
        var sum:Int = sum_user.toInt() //Конвертация из строки в число
        val tax:Double = calcTax(sum)  // Выполнит функцию calcTax, аргументом в которой станет переменная sum
    
        println("For ${sum} ${tax}")
    
    }

--- src/my_program.kt
+++ src/my_program.kt
@@ -1,7 +1,17 @@
-fun main() {
-    fun sum(a:Int=3, b:Int=7):Int{
-        return a+b
-    }
+fun calcTax(sum:Int = 1000): Double{
+    val tax_ratio = 0.13
 
-    print(sum())
+    return tax_ratio*sum
+}
+
+fun main() {
+    val sum_user: String? = readLine()
+
+    if (sum_user == null) return   //Если значение пустое, то возвратит return и покинет функцию ТОЧКА ВХОДА
+    // и приложение завершится
+    var sum:Int = sum_user.toInt() //Конвертация из строки в число
+    val tax:Double = calcTax(sum)  // Выполнит функцию calcTax, аргументом в которой станет переменная sum
+
+    println("For ${sum} ${tax}")
+
 }
